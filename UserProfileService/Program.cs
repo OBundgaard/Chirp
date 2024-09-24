78d541d6b3158fca05aa639e2ff0bd6d4f5bc0e7
Oliver <Oliverbundgaard@live.dk>
2024-09-24 10:45:42 +0200

    Add project files.

---
+++ UserProfileService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserProfileService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, User!");
+        }
+    }
+}
