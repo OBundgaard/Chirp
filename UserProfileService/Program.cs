98300e5266ba2c159d2252412154e88e7b6260ca
Oliver <Oliverbundgaard@live.dk>
2024-10-22 10:18:43 +0200

    Added get and delete user for User service

--- UserProfileService/Program.cs
+++ UserProfileService/Program.cs
@@ -45,4 +45,33 @@ public class Program
 
         Console.WriteLine($"Added '{user.Username}' with display name '{user.DisplayName}' to the database");
     }
+
+    public static async Task<User?> GetUser(int userID)
+    {
+        if (userID < 0)
+            throw new ArgumentException("UserID is below 0");
+
+        if (_context == null)
+            return null;
+
+        return await _context.Users.FirstOrDefaultAsync(u => u.UserID == userID);
+    }
+
+    public static async Task DeleteUser(int userID)
+    {
+        if (_context == null)
+            return;
+
+        User? user = await _context.Users.FirstOrDefaultAsync(u => u.UserID == userID);
+        if (user == null)
+        {
+            Console.WriteLine("User does not exist");
+            return;
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        Console.WriteLine("User deleted successfully");
+    }
 }
